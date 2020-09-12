9f775fbc3cc17f7b71f3a36bdefe4fab5f1a8e4b
biocrystal777 <benedikt.haeusele@uni-konstanz.de>
2020-09-12 22:15:26 +0200

    Added SplitFrames

--- src/GUI/ST2MainFrame.java
+++ src/GUI/ST2MainFrame.java
@@ -8,22 +8,54 @@ import java.awt.*;
  */
 public class ST2MainFrame extends JFrame {
 
-public ST2MainFrame() {
+public ST2MainFrame(){        
         ScreenDims sD = new ScreenDims();
-        this.setSize(sD.width * 19 / 20, sD.height * 19 / 20);
+        this.setSize(sD.width * 17 / 20, sD.height * 17 / 20);
         this.setLocation(sD.width/20, sD.height/20);
-        this.setLayout(null);
+        this.setLayout(null);        
+        this.setupGui();
         this.setVisible(true);
     }
 
-class ScreenDims {
+private ST2MainMenuBar menuBar;
+private ST2MainToolBar toolBar;
+private ST2MainSplitPane mainSplitPane;
+
+private void setupGui(){
+    setLayout(new BorderLayout());
+    setupMenuBar();
+    setupToolBar();
+    setupSplitPane();
+}
+
+private void setupMenuBar() {    
+    menuBar = new ST2MainMenuBar();
+    this.setJMenuBar(menuBar);
+    menuBar.setVisible(true);
+}
+
+private void setupToolBar() {
+    toolBar = new ST2MainToolBar("tools");    
+    this.add(toolBar,BorderLayout.NORTH);
+    toolBar.setVisible(true);   
+}
+
+private void setupSplitPane(){
+    mainSplitPane = new ST2MainSplitPane();
+    this.add(mainSplitPane,BorderLayout.CENTER);
+}
+
+
+class ScreenDims {    
     int width;
     int height;
+
     public ScreenDims() {
         GraphicsDevice gds[] = GraphicsEnvironment.getLocalGraphicsEnvironment().getScreenDevices();
         DisplayMode dm = gds[gds.length-1].getDisplayMode(); 
         width = dm.getWidth();
         height = dm.getHeight();
-    }    
+    }
 }
+
 }   
--- src/STIM2.java
+++ src/STIM2.java
@@ -1,9 +1,7 @@
 import GUI.ST2MainFrame;
 
 public class STIM2 {
-public static void main(String[] args) {
-    System.out.println("Test"); 
+public static void main(String[] args) {    
     ST2MainFrame mainWindow = new ST2MainFrame();
-    int a = 0;
     }
 }
